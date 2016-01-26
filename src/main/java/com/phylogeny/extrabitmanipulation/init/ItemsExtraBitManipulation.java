d44c771ab2f99376925612acc378f6ad0ac39dfc
Phylogeny <molecularphylo@gmail.com>
2016-01-25 20:41:33 -0800

    Added and registered Sculpting Loop.

--- src/main/java/com/phylogeny/extrabitmanipulation/init/ItemsExtraBitManipulation.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/init/ItemsExtraBitManipulation.java
@@ -1,6 +1,7 @@
 package com.phylogeny.extrabitmanipulation.init;
 
 import com.phylogeny.extrabitmanipulation.item.ItemBitWrench;
+import com.phylogeny.extrabitmanipulation.item.ItemSculptingLoop;
 
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -8,11 +9,14 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class ItemsExtraBitManipulation
 {
 	public static Item BitWrench;
+	public static Item SculptingLoop;
 	
 	public static void itemsInit()
 	{
 		BitWrench = new ItemBitWrench();
+		SculptingLoop = new ItemSculptingLoop();
 		GameRegistry.registerItem(BitWrench, ((ItemBitWrench) BitWrench).getName());
+		GameRegistry.registerItem(SculptingLoop, ((ItemSculptingLoop) SculptingLoop).getName());
 	}
 	
 }
