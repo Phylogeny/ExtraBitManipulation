ebdc0a0a4f9b1fa657d5ae3a447090443daec8b5
Phylogeny <molecularphylo@gmail.com>
2016-01-25 20:39:44 -0800

    Moved packet registration to separate class and registered Sculpting Loop model.

--- src/main/java/com/phylogeny/extrabitmanipulation/ExtraBitManipulation.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/ExtraBitManipulation.java
@@ -3,6 +3,7 @@ package com.phylogeny.extrabitmanipulation;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -15,9 +16,10 @@ import net.minecraftforge.fml.relauncher.Side;
 import com.phylogeny.extrabitmanipulation.client.config.ConfigHandlerExtraBitManipulation;
 import com.phylogeny.extrabitmanipulation.client.eventhandler.ClientEventHandler;
 import com.phylogeny.extrabitmanipulation.init.ItemsExtraBitManipulation;
+import com.phylogeny.extrabitmanipulation.init.PacketRegistration;
 import com.phylogeny.extrabitmanipulation.init.RecipesExtraBitManipulation;
 import com.phylogeny.extrabitmanipulation.item.ItemBitWrench;
-import com.phylogeny.extrabitmanipulation.packet.PacketCycleWrench;
+import com.phylogeny.extrabitmanipulation.item.ItemSculptingLoop;
 import com.phylogeny.extrabitmanipulation.reference.Reference;
 
 @Mod(modid = Reference.MOD_ID, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASSPATH, dependencies = "required-after:chiselsandbits")
@@ -31,7 +33,7 @@ public class ExtraBitManipulation
     	ItemsExtraBitManipulation.itemsInit();
 		ConfigHandlerExtraBitManipulation.setUpConfigs(event.getSuggestedConfigurationFile());
 		MinecraftForge.EVENT_BUS.register(new ConfigHandlerExtraBitManipulation());
-		packetNetwork.registerMessage(PacketCycleWrench.Handler.class, PacketCycleWrench.class, 0, Side.SERVER);
+		PacketRegistration.registerPackets();
 	}
 	
 	@EventHandler
@@ -42,8 +44,14 @@ public class ExtraBitManipulation
 		{
 			MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
 			RenderItem renderItem = Minecraft.getMinecraft().getRenderItem();
-			renderItem.getItemModelMesher().register(ItemsExtraBitManipulation.BitWrench, 0, new ModelResourceLocation(Reference.MOD_ID + ":" + ((ItemBitWrench) ItemsExtraBitManipulation.BitWrench).getName(), "inventory"));
+			register(renderItem, ItemsExtraBitManipulation.BitWrench, ((ItemBitWrench) ItemsExtraBitManipulation.BitWrench).getName());
+			register(renderItem, ItemsExtraBitManipulation.SculptingLoop, ((ItemSculptingLoop) ItemsExtraBitManipulation.SculptingLoop).getName());
 		}
+	}
+	
+	private void register(RenderItem renderItem, Item item, String name)
+	{
+		renderItem.getItemModelMesher().register(item, 0, new ModelResourceLocation(Reference.MOD_ID + ":" + name, "inventory"));
 	}
     
 }
