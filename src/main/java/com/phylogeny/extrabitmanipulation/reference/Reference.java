2d07ec9e825f9a4fc34e6a8e10b9690069eee965
Phylogeny <molecularphylo@gmail.com>
2016-01-26 17:14:01 -0800

    Version changed to 1.3.2

--- src/main/java/com/phylogeny/extrabitmanipulation/reference/Reference.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/reference/Reference.java
@@ -6,7 +6,7 @@ public class Reference
 	public static final String GROUP_ID = "extrabitmanipulation";
 	public static final String MOD_NAME = "Extra Bit Manipulation";
 	public static final String MOD_PATH = "com.phylogeny." + GROUP_ID;
-	public static final String VERSION = "1.8.9-1.3.1";
+	public static final String VERSION = "1.8.9-1.3.2";
 	public static final String GUI_FACTORY_CLASSPATH = MOD_PATH + ".client.config.GuiFactoryExtraBitManipulation";
 	
 }
