39c5d9f1d9464e424db0c0bc18add1e267614fea
Phylogeny <molecularphylo@gmail.com>
2016-02-10 06:21:55 -0800

    Added reference class for commonly used NBT keys.

---
+++ src/main/java/com/phylogeny/extrabitmanipulation/reference/NBTKeys.java
@@ -0,0 +1,9 @@
+package com.phylogeny.extrabitmanipulation.reference;
+
+public class NBTKeys
+{
+	public static final String MODE = "mode";
+	public static final String SCULPT_SEMI_DIAMETER = "sculptSemiDiameter";
+	public static final String PAINT_BIT = "paintBit";
+	
+}
