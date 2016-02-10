a44621ca631e4b5449ba3020d9f8fa6340149253
Phylogeny <molecularphylo@gmail.com>
2016-02-10 05:53:34 -0800

    Changed registration helper method and registered models for new items.

--- src/main/java/com/phylogeny/extrabitmanipulation/proxy/ProxyClient.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/proxy/ProxyClient.java
@@ -8,8 +8,7 @@ import net.minecraftforge.common.MinecraftForge;
 
 import com.phylogeny.extrabitmanipulation.client.eventhandler.ClientEventHandler;
 import com.phylogeny.extrabitmanipulation.init.ItemsExtraBitManipulation;
-import com.phylogeny.extrabitmanipulation.item.ItemBitWrench;
-import com.phylogeny.extrabitmanipulation.item.ItemSculptingLoop;
+import com.phylogeny.extrabitmanipulation.item.ItemExtraBitManipulationBase;
 import com.phylogeny.extrabitmanipulation.reference.Reference;
 
 public class ProxyClient extends ProxyCommon
@@ -18,14 +17,24 @@ public class ProxyClient extends ProxyCommon
 	public void registerRenderInformation()
 	{
 		MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
-		RenderItem renderItem = Minecraft.getMinecraft().getRenderItem();
-		register(renderItem, ItemsExtraBitManipulation.BitWrench, ((ItemBitWrench) ItemsExtraBitManipulation.BitWrench).getName());
-		register(renderItem, ItemsExtraBitManipulation.SculptingLoop, ((ItemSculptingLoop) ItemsExtraBitManipulation.SculptingLoop).getName());
+		register(ItemsExtraBitManipulation.DiamondNugget);
+		register(ItemsExtraBitManipulation.BitWrench);
+		register(ItemsExtraBitManipulation.SculptingLoop);
+		register(ItemsExtraBitManipulation.SculptingSquare);
+		register(ItemsExtraBitManipulation.SculptingSpadeCurved);
+		register(ItemsExtraBitManipulation.SculptingSpadeSquared);
+		register(ItemsExtraBitManipulation.BitWrenchHead);
+		register(ItemsExtraBitManipulation.SculptingLoopHead);
+		register(ItemsExtraBitManipulation.SculptingSquareHead);
+		register(ItemsExtraBitManipulation.SculptingSpadeCurvedHead);
+		register(ItemsExtraBitManipulation.SculptingSpadeSquaredHead);
 	}
 	
-	private void register(RenderItem renderItem, Item item, String name)
+	private void register(Item item)
 	{
-		renderItem.getItemModelMesher().register(item, 0, new ModelResourceLocation(Reference.MOD_ID + ":" + name, "inventory"));
+		RenderItem renderItem = Minecraft.getMinecraft().getRenderItem();
+		renderItem.getItemModelMesher().register(item, 0, new ModelResourceLocation(Reference.MOD_ID + ":"
+				+ (((ItemExtraBitManipulationBase) item).getName()), "inventory"));
 	}
 	
 }
