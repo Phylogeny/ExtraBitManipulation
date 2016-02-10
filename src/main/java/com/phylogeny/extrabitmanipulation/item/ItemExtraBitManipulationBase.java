aa9abd192b539ce806940af98a47c6e597595e45
Phylogeny <molecularphylo@gmail.com>
2016-02-10 06:13:01 -0800

    This class was refactored as ItemBitToolBase. This class is now the base for all items; tools, crafting intermediates, and anything else. It now merely puts itself in the EBM creative tab and returns its name.

--- src/main/java/com/phylogeny/extrabitmanipulation/item/ItemExtraBitManipulationBase.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/item/ItemExtraBitManipulationBase.java
@@ -1,68 +1,23 @@
 package com.phylogeny.extrabitmanipulation.item;
 
-import java.util.List;
-
 import com.phylogeny.extrabitmanipulation.client.creativetab.CreativeTabExtraBitManipulation;
 
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.util.StatCollector;
-import net.minecraft.world.World;
 
 public class ItemExtraBitManipulationBase extends Item
 {
 	private String name;
-	public String[] modeTitles;
 	
-	public ItemExtraBitManipulationBase(String name, boolean takesDamage, int maxDamage)
+	public ItemExtraBitManipulationBase(String name)
 	{
 		this.name = name;
 		setUnlocalizedName(name);
 		setCreativeTab(CreativeTabExtraBitManipulation.creativeTab);
-		maxStackSize = 1;
-		if (takesDamage)
-		{
-			setMaxDamage(maxDamage);
-		}
 	}
 	
 	public String getName()
 	{
 		return name;
 	}
-	
-	public void cycleModes(ItemStack stack, boolean forward)
-	{
-		cycleData(stack, "mode", forward, modeTitles.length);
-	}
-	
-	public boolean cycleData(ItemStack stack, String key, boolean forward, int max)
-	{
-		String name = stack.getDisplayName();
-		boolean setTag = !stack.hasTagCompound();
-		if (setTag)
-		{
-			stack.setTagCompound(new NBTTagCompound());
-		}
-		NBTTagCompound nbt = stack.getTagCompound();
-		int mode = (nbt.getInteger(key) + (forward ? 1 : max - 1)) % max;
-		nbt.setInteger(key, mode);
-		return setTag;
-	}
-	
-	@Override
-	public void onCreated(ItemStack stack, World world, EntityPlayer player)
-    {
-		stack.setTagCompound(new NBTTagCompound());
-    }
-	
-	public String getItemStackDisplayName(ItemStack stack)
-    {
-		int mode = stack.hasTagCompound() ? stack.getTagCompound().getInteger("mode") : 0;
-        return ("" + StatCollector.translateToLocal(this.getUnlocalizedNameInefficiently(stack) + ".name")).trim() + " - " + modeTitles[mode];
-    }
 	
 }
