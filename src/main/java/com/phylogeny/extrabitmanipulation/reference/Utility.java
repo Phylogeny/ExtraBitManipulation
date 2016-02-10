47dd26ec7c926965f04e68fa2e136d770f580131
Phylogeny <molecularphylo@gmail.com>
2016-02-10 06:21:09 -0800

    Added utility class for pixel size double/float.

---
+++ src/main/java/com/phylogeny/extrabitmanipulation/reference/Utility.java
@@ -0,0 +1,8 @@
+package com.phylogeny.extrabitmanipulation.reference;
+
+public class Utility
+{
+	public static final float pixelF = 1 / 16.0F;
+	public static final double pixelD = 1 / 16.0D;
+	
+}
