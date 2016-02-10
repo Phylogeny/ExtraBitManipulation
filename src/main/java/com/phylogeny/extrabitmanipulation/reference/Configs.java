a34e1c951b1d51aef48df95cec78509fbb642f21
Phylogeny <molecularphylo@gmail.com>
2016-02-10 05:50:42 -0800

    Refactored preexisting configs, added recipe/property configs for new items, and added general sculpting tool configs.

--- src/main/java/com/phylogeny/extrabitmanipulation/reference/Configs.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/reference/Configs.java
@@ -1,64 +1,49 @@
 package com.phylogeny.extrabitmanipulation.reference;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.phylogeny.extrabitmanipulation.config.Config;
+import com.phylogeny.extrabitmanipulation.config.ConfigShapeRender;
+import com.phylogeny.extrabitmanipulation.config.ConfigShapeRenderPair;
+
+import net.minecraft.item.Item;
+
 public class Configs
 {
+	//SCULPTING SETTINGS
+		public static boolean DISPLAY_NAME_DIAMETER;
+		public static boolean DISPLAY_NAME_USE_METER_UNITS;
+		public static float SEMI_DIAMETER_PADDING;
+		public static boolean PLACE_BITS_IN_INVENTORY;
+		public static boolean DROP_BITS_IN_BLOCKSPACE;
+		public static float BIT_SPAWN_BOX_CONTRACTION;
+		public static boolean DROP_BITS_PER_BLOCK;
+		public static boolean DROP_BITS_AS_FULL_CHISELED_BLOCKS;
+	
 	//ITEM PROPERTIES
-		//BIT WRENCH
-			public static boolean TAKES_DAMAGE_BIT_WRENCH;
-			public static int MAX_DAMAGE_BIT_WRENCH;
-		
-		//SCULPTING LOOP
-			public static boolean TAKES_DAMAGE_SCULPTING_LOOP;
-			public static int MAX_DAMAGE_SCULPTING_LOOP;
-			public static int DEFAULT_REMOVAL_RADIUS;
-			public static int MAX_REMOVAL_RADIUS;
+		public static Map<Item, Config> itemPropertyMap = new HashMap<Item, Config>();
 	
-	//RECIPES
-		//BIT WRENCH
-			public static boolean RECIPE_BIT_WRENCH_IS_ENABLED;
-			public static String[] RECIPE_BIT_WRENCH;
-			public static String[] RECIPE_BIT_WRENCH_DEFAULT = new String[] {
-						"gemDiamond", "", "gemDiamond", "", "ingotIron", "", "", "ingotIron", "" };
-			public static boolean RECIPE_BIT_WRENCH_IS_SHAPED;
-			public static boolean RECIPE_BIT_WRENCH_ORE_DICTIONARY;
-		
-		//SCULPTING LOOP
-			public static boolean RECIPE_SCULPTING_LOOP_IS_ENABLED;
-			public static String[] RECIPE_SCULPTING_LOOP;
-			public static String[] RECIPE_SCULPTING_LOOP_DEFAULT = new String[] {
-						"", "gemDiamond", "", "", "ingotIron", "", "", "ingotIron", "" };
-			public static boolean RECIPE_SCULPTING_LOOP_IS_SHAPED;
-			public static boolean RECIPE_SCULPTING_LOOP_ORE_DICTIONARY;
+	//ITEM RECIPES
+		public static Map<Item, Config> itemRecipeMap = new HashMap<Item, Config>();
 	
-	//RENDER
-		//WRENCH OVERLAYS
-			public static boolean DISABLE_OVERLAYS;
-			public static double ROTATION_PERIOD;
-			public static double MIRROR_PERIOD;
-			public static double MIRROR_AMPLITUDE;
-			public static double TRANSLATION_SCALE_PERIOD;
-			public static double TRANSLATION_DISTANCE;
-			public static double TRANSLATION_OFFSET_DISTANCE;
-			public static double TRANSLATION_FADE_DISTANCE;
-			public static double TRANSLATION_MOVEMENT_PERIOD;
+	//RENDER WRENCH OVERLAYS
+		public static boolean DISABLE_OVERLAYS;
+		public static double ROTATION_PERIOD;
+		public static double MIRROR_PERIOD;
+		public static double MIRROR_AMPLITUDE;
+		public static double TRANSLATION_SCALE_PERIOD;
+		public static double TRANSLATION_DISTANCE;
+		public static double TRANSLATION_OFFSET_DISTANCE;
+		public static double TRANSLATION_FADE_DISTANCE;
+		public static double TRANSLATION_MOVEMENT_PERIOD;
 	
-		//SCULPTING LOOP SPHERE
-			public static boolean RENDER_OUTER_SPHERE;
-			public static boolean RENDER_INNER_SPHERE;
-			public static float OUTER_SPHERE_ALPHA;
-			public static float INNER_SPHERE_ALPHA;
-			public static float SPHERE_RED;
-			public static float SPHERE_GREEN;
-			public static float SPHERE_BLUE;
-			public static float SPHERE_LINE_WIDTH;
-	
-		//SCULPTING LOOP BOX
-			public static boolean RENDER_OUTER_BOX;
-			public static boolean RENDER_INNER_BOX;
-			public static float OUTER_BOX_ALPHA;
-			public static float INNER_BOX_ALPHA;
-			public static float BOX_RED;
-			public static float BOX_GREEN;
-			public static float BOX_BLUE;
-			public static float BOX_LINE_WIDTH;
+	//RENDER SCULPTING TOOL SHAPES
+		public static Map<Item, ConfigShapeRenderPair> itemShapeMap = new HashMap<Item, ConfigShapeRenderPair>();
+		public static ConfigShapeRender[] itemShapes = new ConfigShapeRender[]{
+					new ConfigShapeRender("Bit Removal Bounding Box", true, true, 115, 28, 0, 0, 0, 2.0F),
+					new ConfigShapeRender("Bit Addition Bounding Box", true, false, 115, 28, 0, 0, 0, 2.0F),
+					new ConfigShapeRender("Bit Removal Enveloped Shape", false, true, 38, 115, 0, 0, 255, 2.0F),
+					new ConfigShapeRender("Bit Addition Enveloped Shape", true, false, 38, 115, 0, 0, 255, 2.0F)
+				};
 }
