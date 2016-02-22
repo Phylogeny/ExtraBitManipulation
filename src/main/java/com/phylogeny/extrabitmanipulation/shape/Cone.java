f52d9d4f3e448b508d7cf8bebba549d240229101
Phylogeny <molecularphylo@gmail.com>
2016-02-21 19:41:01 -0800

    Fixed geometry of hollow cones' wall thickness.

--- src/main/java/com/phylogeny/extrabitmanipulation/shape/Cone.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/shape/Cone.java
@@ -1,14 +1,21 @@
 package com.phylogeny.extrabitmanipulation.shape;
 
+import com.phylogeny.extrabitmanipulation.reference.SculptSettings;
+
 import net.minecraft.util.BlockPos;
 
 public class Cone extends SymmetricalShape
 {
+	private float semiDiameterInset2;
 	
 	@Override
-	public void init(float centerX, float centerY, float centerZ, float radius, float wallThickness, boolean isSolid)
+	public void init(float centerX, float centerY, float centerZ, float radius)
 	{
-		super.init(centerX, centerY, centerZ, radius, wallThickness, isSolid);
+		super.init(centerX, centerY, centerZ, radius);
+		semiDiameterInset2 = semiDiameterInset;
+		float r = semiDiameter;
+		float d = r * 2;
+		semiDiameterInset = r - (float) ((Math.sqrt(r * r + d * d) * SculptSettings.WALL_THICKNESS) / r);
 	}
 	
 	@Override
@@ -19,15 +26,20 @@ public class Cone extends SymmetricalShape
 		float dx = getBitPosDiffX(pos, i, centerX);
 		float dz = getBitPosDiffZ(pos, k, centerZ);
 		double dist = Math.sqrt(dx * dx + dz * dz);
-		boolean inShape = isPointInCircle(y, centerY, semiDiameter, dist);
-		return isSolid ? inShape : inShape && !(isPointInCircle(y, centerY, semiDiameterInset, dist)
-				&& !(isPointOffLine(y, centerY, semiDiameterInset)));
+		boolean inShape = isPointInCircle(y, centerY, semiDiameter, semiDiameter, dist);
+		return SculptSettings.SCULPT_HOLLOW_SHAPE ? inShape && !(isPointInCircle(y, centerY, semiDiameter, semiDiameterInset, dist)
+				&& (SculptSettings.OPEN_ENDS || !(isPointOffLine(y, centerY, semiDiameterInset2)))) : inShape;
 	}
 	
-	private boolean isPointInCircle(float val, float centerVal, float semiDiameter, double dist)
+	private boolean isPointOffLine(float val, float centerVal)
+	{
+		return val < centerVal - semiDiameterInset || val > centerVal + semiDiameterInset2;
+	}
+	
+	private boolean isPointInCircle(float val, float centerVal, float semiDiameter, float semiDiameter2, double dist)
 	{
 		float d = semiDiameter * 2;
-		return dist <= ((centerVal + semiDiameter - val) * semiDiameter) / d;
+		return dist <= ((centerVal + semiDiameter2 - val) * semiDiameter) / d;
 	}
 	
 }
--- src/main/java/com/phylogeny/extrabitmanipulation/shape/ConeElliptic.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/shape/ConeElliptic.java
@@ -1,15 +1,23 @@
 package com.phylogeny.extrabitmanipulation.shape;
 
+import com.phylogeny.extrabitmanipulation.reference.SculptSettings;
+
 import net.minecraft.util.BlockPos;
 
 public class ConeElliptic extends AsymmetricalShape
 {
+	private float aInset2, bInset2, cInset2;
 	
 	@Override
 	public void init(float centerX, float centerY, float centerZ,
-			float a, float b, float c, float wallThickness, boolean isSolid)
+			float a, float b, float c)
 	{
-		super.init(centerX, centerY, centerZ, a, b, c, wallThickness, isSolid);
+		super.init(centerX, centerY, centerZ, a, b, c);
+		float h = b * 2;
+		float hsq = h * h;
+		aInset2 = b - (float) ((Math.sqrt(a * a + hsq) * SculptSettings.WALL_THICKNESS) / a);
+		cInset2 = b - (float) ((Math.sqrt(c * c + hsq) * SculptSettings.WALL_THICKNESS) / c);
+		bInset2 = Math.max(aInset2, cInset2);
 	}
 	
 	@Override
@@ -19,17 +27,23 @@ public class ConeElliptic extends AsymmetricalShape
 		if (isPointOffLine(y, centerY, b)) return false;
 		float dx = getBitPosDiffX(pos, i, centerX);
 		float dz = getBitPosDiffZ(pos, k, centerZ);
-		boolean inShape = isPointInEllipse(y, centerY, b, a, c, dx, dz);
-		return isSolid ? inShape : inShape && !(isPointInEllipse(y, centerY, bInset, aInset, cInset, dx, dz)
-				&& !isPointOffLine(y, centerY, bInset));
+		boolean inShape = isPointInEllipse(y, centerY, b, b, b, a, c, dx, dz);
+		return SculptSettings.SCULPT_HOLLOW_SHAPE ? inShape && !(isPointInEllipse(y, centerY, b, aInset2, cInset2, a, c, dx, dz)
+				&& !isPointOffLine(y, centerY, bInset, bInset2)) : inShape;
 	}
 	
-	private boolean isPointInEllipse(float val, float centerVal, float semiDiameter, float s1, float s2, float dv1, float dv2)
+	private boolean isPointOffLine(float val, float centerVal, float semiDiameter, float semiDiameter2)
+	{
+		return (!SculptSettings.OPEN_ENDS && val < centerVal - semiDiameter) || val > centerVal + semiDiameter2;
+	}
+	
+	private boolean isPointInEllipse(float val, float centerVal, float semiDiameter,
+			float semiDiameter2, float semiDiameter3, float s1, float s2, float dv1, float dv2)
 	{
 		float d = semiDiameter * 2;
-		float h = centerVal + semiDiameter - val;
-		float s3 = (h * s1) / d;
-		float s4 = (h * s2) / d;
+		float h = centerVal - val;
+		float s3 = ((h + semiDiameter2) * s1) / d;
+		float s4 = ((h + semiDiameter3) * s2) / d;
 		return isPointInEllipse(dv1, dv2, s3, s4);
 	}
 	
