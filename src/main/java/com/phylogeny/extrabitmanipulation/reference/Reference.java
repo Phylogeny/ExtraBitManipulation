04c461a1d3e6b04095ae3a692b26052011cb7045
Phylogeny <molecularphylo@gmail.com>
2016-04-07 17:18:42 -0700

    Version changed to 2.0.0 for C&B 8.x

--- src/main/java/com/phylogeny/extrabitmanipulation/reference/Reference.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/reference/Reference.java
@@ -6,7 +6,7 @@ public class Reference
 	public static final String GROUP_ID = "extrabitmanipulation";
 	public static final String MOD_NAME = "Extra Bit Manipulation";
 	public static final String MOD_PATH = "com.phylogeny." + GROUP_ID;
-	public static final String VERSION = "1.8.9-1.4.0";
+	public static final String VERSION = "1.8.9-2.0.0";
 	public static final String CLIENT_CLASSPATH = MOD_PATH + ".proxy.ProxyClient";
 	public static final String COMMON_CLASSPATH = MOD_PATH + ".proxy.ProxyCommon";
 	public static final String GUI_FACTORY_CLASSPATH = MOD_PATH + ".client.config.GuiFactoryExtraBitManipulation";
