a42a0fcaa237bb9a32a4c105ce6f8deb873d09bf
Phylogeny <molecularphylo@gmail.com>
2016-11-11 09:00:49 -0800

    Made the following changes to the bit mapping GUI: 1) removed case sensitivity when searching for block states; 2) improved the rendering of block states; 3) added throwing of a crash report when an unexpected error occurs while rendering block states with models that have built-in renderers; 4) added safeguards against models that throw NPEs when retrieving their quad lists (*pokes Tema* extrautils2:MoonStone - com.rwtema.extrautils2.backend.model.PassthruModelBlock.func_188616_a(PassthruModelBlock.java:37)).

--- src/main/java/com/phylogeny/extrabitmanipulation/client/renderer/RenderState.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/renderer/RenderState.java
@@ -2,11 +2,13 @@ package com.phylogeny.extrabitmanipulation.client.renderer;
 
 import java.util.List;
 
+import com.phylogeny.extrabitmanipulation.reference.Reference;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockStairs;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BlockRendererDispatcher;
+import net.minecraft.client.renderer.BlockModelShapes;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
@@ -19,9 +21,13 @@ import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.texture.TextureUtil;
 import net.minecraft.client.renderer.tileentity.TileEntityItemStackRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.crash.CrashReport;
+import net.minecraft.crash.CrashReportCategory;
+import net.minecraft.crash.ICrashReportDetail;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ReportedException;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.client.model.pipeline.LightUtil;
@@ -30,10 +36,124 @@ public class RenderState
 {
 	private static final ResourceLocation RES_ITEM_GLINT = new ResourceLocation("textures/misc/enchanted_item_glint.png");
 	
-	public static void renderStateModelIntoGUI(IBlockState state, int x, int y)
+	public static void renderStateIntoGUI(final IBlockState state, int x, int y)
 	{
-		BlockRendererDispatcher rendererDispatcher = Minecraft.getMinecraft().getBlockRendererDispatcher();
-		IBakedModel model = rendererDispatcher.getBlockModelShapes().getModelForState(state);
+		BlockModelShapes blockModelShapes = Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelShapes();
+		IBakedModel model = blockModelShapes.getModelForState(state);
+		boolean emptyModel;
+		try
+		{
+			boolean missingModel = isMissingModel(blockModelShapes, model);
+			emptyModel = missingModel || model.getQuads(state, null, 0L).isEmpty();
+			if (!missingModel && emptyModel)
+			{
+				for (EnumFacing enumfacing : EnumFacing.values())
+				{
+					if (!model.getQuads(state, enumfacing, 0L).isEmpty())
+					{
+						emptyModel = false;
+						break;
+					}
+				}
+			}
+		}
+		catch (NullPointerException e)
+		{
+			emptyModel = true;
+		}
+		final Block block = state.getBlock();
+		ItemStack stack = new ItemStack(block, 1, block.getMetaFromState(state));
+		if (isNullItem(block, stack))
+			stack = null;
+		
+		boolean isVanillaChest = block == Blocks.CHEST || block == Blocks.ENDER_CHEST || block == Blocks.TRAPPED_CHEST;
+		if (stack != null && emptyModel)
+		{
+			model = getItemModelWithOverrides(stack);
+			if (!isVanillaChest && isMissingModel(blockModelShapes, model))
+			{
+				stack = new ItemStack(block);
+				if (isNullItem(block, stack))
+					stack = null;
+				
+				if (stack != null)
+					model = getItemModelWithOverrides(stack);
+			}
+		}
+		boolean renderAsTileEntity = stack != null && (model.isBuiltInRenderer() || isVanillaChest);
+		try
+		{
+			renderStateModelIntoGUI(state, block, model, stack, renderAsTileEntity, x, y);
+		}
+		catch (Throwable throwable)
+		{
+			CrashReport crashreport = CrashReport.makeCrashReport(throwable, "Rendering block state in " + Reference.MOD_ID + " bit mapping GUI");
+			CrashReportCategory crashreportcategory = crashreport.makeCategory("Block state being rendered");
+			crashreportcategory.setDetail("Block State", new ICrashReportDetail<String>()
+			{
+				@Override
+				public String call() throws Exception
+				{
+					return String.valueOf(state);
+				}
+			});
+			if (stack != null)
+			{
+				final ItemStack stack2 = stack.copy();
+				crashreportcategory.setDetail("State's Item Type", new ICrashReportDetail<String>()
+				{
+					@Override
+					public String call() throws Exception
+					{
+						return String.valueOf(stack2.getItem());
+					}
+				});
+				crashreportcategory.setDetail("State's Item Aux", new ICrashReportDetail<String>()
+				{
+					@Override
+					public String call() throws Exception
+					{
+						return String.valueOf(stack2.getMetadata());
+					}
+				});
+				crashreportcategory.setDetail("State's Item NBT", new ICrashReportDetail<String>()
+				{
+					@Override
+					public String call() throws Exception
+					{
+						return String.valueOf(stack2.getTagCompound());
+					}
+				});
+				crashreportcategory.setDetail("State's Item Foil", new ICrashReportDetail<String>()
+				{
+					@Override
+					public String call() throws Exception
+					{
+						return String.valueOf(stack2.hasEffect());
+					}
+				});
+			}
+			throw new ReportedException(crashreport);
+		}
+	}
+	
+	private static IBakedModel getItemModelWithOverrides(ItemStack stack)
+	{
+		return Minecraft.getMinecraft().getRenderItem().getItemModelWithOverrides(stack, null, Minecraft.getMinecraft().thePlayer);
+	}
+	
+	private static boolean isNullItem(final Block block, ItemStack stack)
+	{
+		return stack.getItem() == null || block == Blocks.STANDING_BANNER || block == Blocks.BARRIER;
+	}
+	
+	private static boolean isMissingModel(BlockModelShapes blockModelShapes, IBakedModel model)
+	{
+		return model.equals(blockModelShapes.getModelManager().getMissingModel());
+	}
+	
+	private static void renderStateModelIntoGUI(IBlockState state, Block block, IBakedModel model, ItemStack stack, boolean renderAsTileEntity, int x, int y)
+	{
 		TextureManager textureManager = Minecraft.getMinecraft().getTextureManager();
 		GlStateManager.pushMatrix();
 		textureManager.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
@@ -46,8 +166,7 @@ public class RenderState
 		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 		setupGuiTransform(x, y, model);
 		model = ForgeHooksClient.handleCameraTransforms(model, ItemCameraTransforms.TransformType.HEAD, false);
-		renderState(state, model);
-		GlStateManager.disableAlpha();
+		renderState(state, block, model, stack, renderAsTileEntity);
 		GlStateManager.disableRescaleNormal();
 		GlStateManager.disableLighting();
 		GlStateManager.popMatrix();
@@ -55,16 +174,11 @@ public class RenderState
 		textureManager.getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
 	}
 	
-	private static void renderState(IBlockState state, IBakedModel model)
+	private static void renderState(IBlockState state, Block block, IBakedModel model, ItemStack stack, boolean renderAsTileEntity)
 	{
-		Block block = state.getBlock();
-		ItemStack stack = new ItemStack(block, 1, block.getMetaFromState(state));
-		if (stack.getItem() == null)
-			stack = null;
-		
 		GlStateManager.pushMatrix();
 		GlStateManager.scale(0.65F, 0.65F, 0.65F);
-		if (stack != null && (model.isBuiltInRenderer() || block == Blocks.CHEST || block == Blocks.ENDER_CHEST || block == Blocks.TRAPPED_CHEST))
+		if (renderAsTileEntity)
 		{
 			GlStateManager.rotate(45, 0, 1, 0);
 			GlStateManager.rotate(30, 1, 0, 1);
@@ -95,7 +209,7 @@ public class RenderState
 	
 	private static void setupGuiTransform(int x, int y, IBakedModel model)
 	{
-		GlStateManager.translate(x + 6, y + 2, 100.0F + Minecraft.getMinecraft().getRenderItem().zLevel);
+		GlStateManager.translate(x + 6, y + 2, 100.0F + Minecraft.getMinecraft().getRenderItem().zLevel + 50);
 		GlStateManager.translate(8.0F, 8.0F, 0.0F);
 		GlStateManager.scale(1.0F, -1.0F, 1.0F);
 		GlStateManager.scale(16.0F, 16.0F, 16.0F);
@@ -114,12 +228,19 @@ public class RenderState
 		Tessellator tessellator = Tessellator.getInstance();
 		VertexBuffer vertexbuffer = tessellator.getBuffer();
 		vertexbuffer.begin(7, DefaultVertexFormats.ITEM);
-		for (EnumFacing enumfacing : EnumFacing.values())
+		try
 		{
-			renderQuads(vertexbuffer, model.getQuads(state, enumfacing, 0L), color, stack);
+			for (EnumFacing enumfacing : EnumFacing.values())
+			{
+				renderQuads(vertexbuffer, model.getQuads(state, enumfacing, 0L), color, stack);
+			}
+			renderQuads(vertexbuffer, model.getQuads(state, null, 0L), color, stack);
 		}
-		renderQuads(vertexbuffer, model.getQuads(state, null, 0L), color, stack);
-		tessellator.draw();
+		catch (NullPointerException e) {}
+		finally
+		{
+			tessellator.draw();
+		}
 	}
 	
 	private static void renderEffect(IBlockState state, IBakedModel model)
