77847d94568255fc248cf8f46f78c40101d9db47
Phylogeny <molecularphylo@gmail.com>
2017-01-16 13:15:54 -0800

    Added JEI description pages for Chisels & Bits designs and bit items.

--- src/main/java/com/phylogeny/extrabitmanipulation/api/jei/JustEnoughItemsPlugin.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/api/jei/JustEnoughItemsPlugin.java
@@ -15,10 +15,13 @@ import com.phylogeny.extrabitmanipulation.reference.Reference;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 import mezz.jei.api.BlankModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
 import mezz.jei.util.Translator;
+import mod.chiselsandbits.core.ChiselsAndBits;
+import mod.chiselsandbits.registry.ModItems;
 
 @JEIPlugin
 public class JustEnoughItemsPlugin extends BlankModPlugin
@@ -36,6 +39,9 @@ public class JustEnoughItemsPlugin extends BlankModPlugin
 		ItemStack modelingStack = addDescription(registry, ItemsExtraBitManipulation.modelingTool);
 		List<ItemStack> sculptingStacks = addDescription(registry, "sculpting", ItemsExtraBitManipulation.sculptingLoop, ItemsExtraBitManipulation.sculptingSquare,
 				ItemsExtraBitManipulation.sculptingSpadeCurved, ItemsExtraBitManipulation.sculptingSpadeSquared);
+		ModItems items = ChiselsAndBits.getItems();
+		addDescription(registry, items.itemBlockBit);
+		addDescription(registry, "designs", items.itemMirrorprint, items.itemNegativeprint, items.itemPositiveprint);
 		registry.addRecipeCategories(new ShapeInfoRecipeCategory(registry.getJeiHelpers().getGuiHelper()));
 		registry.addRecipeHandlers(new ShapeInfoRecipeHandler());
 		registry.addRecipes(ShapeInfoRecipe.create(registry.getJeiHelpers().getGuiHelper(), sculptingStacks));
@@ -46,7 +52,7 @@ public class JustEnoughItemsPlugin extends BlankModPlugin
 	
 	private ItemStack addDescription(IModRegistry registry, Item item)
 	{
-		ItemStack stack = new ItemStack(item);
+		ItemStack stack = new ItemStack(item, 1, OreDictionary.WILDCARD_VALUE);
 		registry.addDescription(stack, "jei.description." + item.getRegistryName());
 		return stack;
 	}
