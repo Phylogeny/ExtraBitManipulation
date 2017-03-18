effc4294350b010f8a79ea560c68a794b0cbe494
Phylogeny <molecularphylo@gmail.com>
2017-03-18 01:18:39 -0700

    Fixed bug where rectangular pyramids could not be drawn when facing in a negative axial direction. Fixes #11

--- src/main/java/com/phylogeny/extrabitmanipulation/shape/PyramidRectangular.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/shape/PyramidRectangular.java
@@ -7,8 +7,8 @@ public class PyramidRectangular extends SlopedAsymmetricalShape
 	protected boolean isPointIn2DShape(float val, float semiDiameter1, float semiDiameter2, float dv1, float dv2)
 	{
 		float h = centerY - val;
-		float s1 = ((h + (inverted ? -semiDiameter1 : semiDiameter1)) * a) / height;
-		float s2 = ((h + (inverted ? -semiDiameter2 : semiDiameter2)) * c) / height;
+		float s1 = Math.abs(((h + (inverted ? -semiDiameter1 : semiDiameter1)) * a) / height);
+		float s2 = Math.abs(((h + (inverted ? -semiDiameter2 : semiDiameter2)) * c) / height);
 		return isPointInRectangle(dv1, dv2, s1, s2);
 	}
 	
