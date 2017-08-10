81f99f02c1f64a4502be07b52ab3df5aaabfad9b
Phylogeny <molecularphylo@gmail.com>
2017-08-10 04:33:38 -0700

    Refactored/optimized Chiseled Armor recipe.

--- src/main/java/com/phylogeny/extrabitmanipulation/init/RecipesExtraBitManipulation.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/init/RecipesExtraBitManipulation.java
@@ -4,7 +4,10 @@ import mod.chiselsandbits.core.ChiselsAndBits;
 import mod.chiselsandbits.registry.ModItems;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.oredict.OreDictionary;
@@ -29,24 +32,20 @@ public class RecipesExtraBitManipulation
 		registerChiseledArmorRecipes(event, ItemsExtraBitManipulation.chiseledBoots, Items.DIAMOND_BOOTS, 272);
 	}
 	
-	private static void registerChiseledArmorRecipes(RegistryEvent.Register<IRecipe> event, Item output, Item inputArmor, int bitCost)
+	private static void registerChiseledArmorRecipes(RegistryEvent.Register<IRecipe> event, Item itemChiseled, Item itemVanilla, int bitCost)
+	{
+		registerChiseledArmorRecipe(event, itemChiseled, itemVanilla, bitCost);
+		registerChiseledArmorRecipe(event, itemVanilla, itemChiseled, bitCost);
+	}
+	
+	private static void registerChiseledArmorRecipe(RegistryEvent.Register<IRecipe> event, Item output, Item input, int bitCost)
 	{
 		ModItems items = ChiselsAndBits.getItems();
-		registerChiseledArmorRecipes(event, output, inputArmor, items.itemChiselStone, bitCost);
-		registerChiseledArmorRecipes(event, output, inputArmor, items.itemChiselIron, bitCost);
-		registerChiseledArmorRecipes(event, output, inputArmor, items.itemChiselGold, bitCost);
-		registerChiseledArmorRecipes(event, output, inputArmor, items.itemChiselDiamond, bitCost);
-	}
-	
-	private static void registerChiseledArmorRecipes(RegistryEvent.Register<IRecipe> event, Item output, Item input, Item inputChisel, int bitCost)
-	{
-		registerChiseledArmorRecipe(event, output, input, inputChisel, bitCost);
-		registerChiseledArmorRecipe(event, input, output, inputChisel, bitCost);
-	}
-	
-	private static void registerChiseledArmorRecipe(RegistryEvent.Register<IRecipe> event, Item output, Item input, Item inputChisel, int bitCost)
-	{
-		event.getRegistry().register(new RecipeChiseledArmor(output, input, inputChisel, bitCost));
+		NonNullList<Ingredient> ingredients = NonNullList.<Ingredient>create();
+		ingredients.add(Ingredient.fromStacks(new ItemStack(input)));
+		ingredients.add(Ingredient.fromStacks(new ItemStack(items.itemChiselStone), new ItemStack(items.itemChiselIron),
+				new ItemStack(items.itemChiselGold), new ItemStack(items.itemChiselDiamond)));
+		event.getRegistry().register(new RecipeChiseledArmor(ingredients, output, input, bitCost));
 	}
 	
 }
--- src/main/java/com/phylogeny/extrabitmanipulation/recipe/RecipeChiseledArmor.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/recipe/RecipeChiseledArmor.java
@@ -2,81 +2,37 @@ package com.phylogeny.extrabitmanipulation.recipe;
 
 import java.util.Random;
 
-import javax.annotation.Nullable;
-
 import mod.chiselsandbits.items.ItemChisel;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.event.ForgeEventFactory;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 import com.phylogeny.extrabitmanipulation.reference.Reference;
 
-public class RecipeChiseledArmor implements IRecipe
+public class RecipeChiseledArmor extends ShapelessOreRecipe
 {
-	private final ItemStack output;
-	public final NonNullList<Ingredient> ingredients;
 	private Random rand = new Random();
-	private ItemStack chiselRemaining = ItemStack.EMPTY;
-	private Item input;
 	private int bitCost;
-	private ResourceLocation registryName;
 	
-	public RecipeChiseledArmor(Item output, Item input, Item inputChisel, int bitCost)
+	public RecipeChiseledArmor(NonNullList<Ingredient> ingredients, Item output, Item input, int bitCost)
 	{
-		ingredients = NonNullList.<Ingredient>create();
-		ingredients.add(Ingredient.fromStacks(new ItemStack(input)));
-		ingredients.add(Ingredient.fromStacks(new ItemStack(inputChisel)));
-		this.output = new ItemStack(output);
-		this.input = input;
+		super(null, ingredients, new ItemStack(output));
 		this.bitCost = bitCost;
-		registryName = new ResourceLocation(Reference.MOD_ID, "chiseledarmor" + "_"
-				+ getItemName(inputChisel) + "+" + getItemName(input) + "=" + getItemName(output));
+		setRegistryName(Reference.MOD_ID, "chiseledarmor" + "_chisel" + "+" + getItemName(input) + "=" + getItemName(output));
 	}
 	
 	private String getItemName(Item item)
 	{
 		ResourceLocation name = item.getRegistryName();
 		return name != null ? name.toString().substring(name.toString().indexOf(":") + 1).replace("_", "") : "";
-	}
-	
-	@Override
-	public boolean matches(InventoryCrafting inv, World world)
-	{
-		ItemStack chisel = ItemStack.EMPTY;
-		boolean foundArmor = false;
-		int itemCount = 0;
-		for (int x = 0; x < inv.getWidth(); x++)
-		{
-			for (int y = 0; y < inv.getHeight(); y++)
-			{
-				ItemStack stack = inv.getStackInRowAndColumn(x, y);
-				if (stack.isEmpty())
-					continue;
-				
-				itemCount++;
-				boolean isChisel = stack.getItem() instanceof ItemChisel;
-				if (isChisel)
-					chisel = stack;
-				
-				boolean isArmor = stack.getItem() == input;
-				if (isArmor)
-					foundArmor = true;
-				
-				if (!isChisel && !isArmor)
-					return false;
-			}
-		}
-		if (!chisel.isEmpty() && foundArmor && itemCount == 2)
-		{
-			chiselRemaining = chisel;
-			return true;
-		}
-		return false;
 	}
 	
 	@Override
@@ -88,58 +44,18 @@ public class RecipeChiseledArmor implements IRecipe
 			ItemStack stack = inv.getStackInSlot(i);
 			if (stack.getItem() instanceof ItemChisel)
 			{
-				if (chiselRemaining.isItemStackDamageable() && chiselRemaining.attemptDamageItem(bitCost, rand, null))
+				ItemStack chiselRemaining = stack.copy();
+				EntityPlayer player = ForgeHooks.getCraftingPlayer();
+				if (chiselRemaining.attemptDamageItem(bitCost, rand, player instanceof EntityPlayerMP ? (EntityPlayerMP) player : null))
+				{
+					ForgeEventFactory.onPlayerDestroyItem(player, stack, null);
 					chiselRemaining = ItemStack.EMPTY;
-				
-				remainingItems.set(i, chiselRemaining.copy());
+				}
+				remainingItems.set(i, chiselRemaining);
 				break;
 			}
 		}
 		return remainingItems;
-	}
-	
-	@Override
-	public IRecipe setRegistryName(ResourceLocation name)
-	{
-		registryName = name;
-		return this;
-	}
-	
-	@Override
-	@Nullable
-	public ResourceLocation getRegistryName()
-	{
-		return registryName;
-	}
-	
-	@Override
-	public Class<IRecipe> getRegistryType()
-	{
-		return IRecipe.class;
-	}
-	
-	@Override
-	public ItemStack getCraftingResult(InventoryCrafting inv)
-	{
-		return this.output.copy();
-	}
-	
-	@Override
-	public boolean canFit(int width, int height)
-	{
-		return width * height >= ingredients.size();
-	}
-	
-	@Override
-	public ItemStack getRecipeOutput()
-	{
-		return output;
-	}
-	
-	@Override
-	public NonNullList<Ingredient> getIngredients()
-	{
-		return ingredients;
 	}
 	
 }
