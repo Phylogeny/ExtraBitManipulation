3bab65fb564d1993536fa7a12c70935a039f06c4
Phylogeny <molecularphylo@gmail.com>
2018-10-14 01:48:49 -0700

    Updated capability registration to use non-deprecated method.

--- src/main/java/com/phylogeny/extrabitmanipulation/proxy/ProxyCommon.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/proxy/ProxyCommon.java
@@ -60,7 +60,7 @@ public class ProxyCommon implements IGuiHandler
 		MinecraftForge.EVENT_BUS.register(new BlocksExtraBitManipulation());
 		MinecraftForge.EVENT_BUS.register(new RecipesExtraBitManipulation());
 		MinecraftForge.EVENT_BUS.register(new ChiseledArmorSlotsEventHandler());
-		CapabilityManager.INSTANCE.register(IChiseledArmorSlotsHandler.class, new ChiseledArmorSlotsStorage(), ChiseledArmorSlotsHandler.class);
+		CapabilityManager.INSTANCE.register(IChiseledArmorSlotsHandler.class, new ChiseledArmorSlotsStorage(), ChiseledArmorSlotsHandler::new);
 		PacketRegistration.registerPackets();
 		ResourceLocation name = new ResourceLocation(Reference.MOD_ID, "entity_bit");
 		EntityRegistry.registerModEntity(name, EntityBit.class, name.toString(), 0, ExtraBitManipulation.instance, 64, 3, false);
